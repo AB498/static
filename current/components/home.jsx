ea899d4442c2d705bf9bd233ccf2c65797b971e6
AB498 <abcd49800@gmail.com>
2024-08-28 12:55:01 +0000

    Aug 28, 2024, 6:55 PM

--- current/components/home.jsx
+++ current/components/home.jsx
@@ -2,7 +2,7 @@
     let Component = (props) => {
         let { children } = props || {};
         return <div>
-            <div>Home{uuid().slice(0, 5)}</div>
+            <div>Homes{uuid().slice(0, 5)}</div>
             {children}
         </div>
     }
