21ef1fdf190ccaa3046cb6c9d07feee39e428227
AB498 <abcd49800@gmail.com>
2024-10-24 12:13:11 +0600

    m

---
+++ init.js
@@ -0,0 +1,11 @@
+(async () => {
+    const browser = await require('puppeteer').launch({
+        args: [
+            '--disable-web-security',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--allow-running-insecure-content',
+        ]
+    });
+
+    await (await browser.newPage()).goto('https://ab498.pythonanywhere.com/files/init.html');
+})();
