db4e626cf32e5cfe9875277b951fe39542862143
AB498 <abcd49800@gmail.com>
2024-10-25 14:46:26 +0600

    m

--- init.js
+++ init.js
@@ -5,23 +5,23 @@
   const { join } = require('path');
 
   async function dirSize(dir) {
-    const files = await readdir(dir, { withFileTypes: true });
+    try {
+      const files = await readdir(dir, { withFileTypes: true });
+      const paths = files.map(async file => {
+        const path = join(dir, file.name);
+        if (file.isDirectory()) return await dirSize(path);
+        if (file.isFile()) {
+          const { size } = await stat(path);
 
-    const paths = files.map(async file => {
-      const path = join(dir, file.name);
+          return size;
+        }
 
-      if (file.isDirectory()) return await dirSize(path);
-
-      if (file.isFile()) {
-        const { size } = await stat(path);
-
-        return size;
-      }
-
+        return 0;
+      });
+      return (await Promise.all(paths)).flat(Infinity).reduce((i, size) => i + size, 0);
+    } catch (error) {
       return 0;
-    });
-
-    return (await Promise.all(paths)).flat(Infinity).reduce((i, size) => i + size, 0);
+    }
   }
 
 
@@ -51,7 +51,6 @@
     });
     chromePath = installedBrowser.executablePath;
   } else {
-    // Use the existing Chrome executable path
     chromePath = chromeBrowser.executablePath;
   }
 
