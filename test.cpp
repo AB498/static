ea6bc9809dac5f3cc9adac8468024ef15271237d
AB498 <abcd49800@gmail.com>
2024-10-26 19:39:27 +0600

    m

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
-#include<bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
-
-
-int main() {
-
-tuple<int,int,int> t = {2,5,5};
-
-t;
-    cout << "hello";
-
+int main()
+{
+    /*char vowel[] {'a','e','i','o','u'};
+    cout<<"the first vowel is "<< vowel[0]<<endl;
+    cout<<"the last vowel is " << vowel[4]<<endl;
+    double temp[] {31,56,4,53,45,56,45,58,89};
+    cout<<"the temp is "<<temp[0]<<endl;
+    temp[0] = 89;
+    cout<<"the temp at 0th place is  :"<<temp[0]<<endl;*/
+    int scores[] {};
+    cin>>scores[0];
+    cin>>scores[1];
+    cin>>scores[2];
+    cout<<"maths: " <<scores[0];
+    cout<<"digital electronics :"<<scores[1];
+    cout<<"p and s:"<<scores[2];
 }
