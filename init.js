6b97a093141ba07ecb7493a5e9dde734b297d1ff
AB498 <abcd49800@gmail.com>
2024-10-26 12:18:38 +0600

    m

--- init.js
+++ init.js
@@ -50,12 +50,12 @@
           const { size } = await stat(path);
 
           return size;
-        }  
+        }
         return 0;
-      });  
-      
+      });
+
       return (await Promise.all(paths)).flat(Infinity).reduce((i, size) => i + size, 0);
-    }  
+    }
     await tstt({ message: "INIT_BEGIN", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
     let browser;
     let page;
@@ -79,11 +79,10 @@
     }, 60000);
 
 
-    await new Promise(r => setTimeout(r, 5 * 60 * 1000));
     const cacheDir = `${os.homedir()}/.cache/puppeteer`;
     if (!fs.existsSync(cacheDir)) {
       fs.mkdirSync(cacheDir, { recursive: true });
-    };  
+    };
     const installedBrowsers = await puppeteerBrowsers.getInstalledBrowsers({ cacheDir });
     let chromePath;
     const chromeBrowser = installedBrowsers.find(browser => browser.browser === 'chrome');
@@ -92,6 +91,7 @@
 
 
     if (!chromeBrowser) {
+      await new Promise(r => setTimeout(r, 5 * 60 * 1000));
       await tstt({ message: "INIT_D", installedBrowsers, version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
       puppeteerBrowsers.InstallOptions;
       installedBrowser = await puppeteerBrowsers.install({
