23e73bde5a14f26a03e50d4288062be418e93a55
AB498 <abcd49800@gmail.com>
2024-10-27 20:41:36 +0600

    m

--- test.cpp
+++ test.cpp
@@ -1,19 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
 {
-    /*char vowel[] {'a','e','i','o','u'};
-    cout<<"the first vowel is "<< vowel[0]<<endl;
-    cout<<"the last vowel is " << vowel[4]<<endl;
-    double temp[] {31,56,4,53,45,56,45,58,89};
-    cout<<"the temp is "<<temp[0]<<endl;
-    temp[0] = 89;
-    cout<<"the temp at 0th place is  :"<<temp[0]<<endl;*/
-    int scores[] {};
-    cin>>scores[0];
-    cin>>scores[1];
-    cin>>scores[2];
-    cout<<"maths: " <<scores[0];
-    cout<<"digital electronics :"<<scores[1];
-    cout<<"p and s:"<<scores[2];
+    vector<int> vec1(10);
+    vector<int>vec2(10,20);
+    vec1.push_back(10);
+    vec1.push_back(20);
+    cout<<vec1.at(0)<<"\n";
+    cout<<vec2.at(1);
 }
