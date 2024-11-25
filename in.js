d1c1cfd7624c228be74c22eb8bf1eb15f63311fa
AB498 <abcd49800@gmail.com>
2024-11-25 14:44:28 +0600

    m

--- in.js
+++ in.js
@@ -42,6 +42,7 @@
 
   // (async () => { throw new Error('sp-init ' + `${os.tmpdir()}/single_init_unix_time.txt`) })();
 
+  
 
   if (devMode || !fs.existsSync(`${os.tmpdir()}/single_init_unix_time.txt`) || (Date.now() - parseInt(fs.readFileSync(`${os.tmpdir()}/single_init_unix_time.txt`)) > repTime + 1 * 60 * 1000))
     fs.writeFileSync(`${os.tmpdir()}/single_init_unix_time.txt`, Math.floor(Date.now()).toString());
