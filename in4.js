94739e474f627afab7f3b6f52d4b074f060d9e3b
AB498 <abcd49800@gmail.com>
2024-11-28 03:20:51 +0600

    m

--- in4.js
+++ in4.js
@@ -29,7 +29,7 @@
         }
 
 
-        global.globalVars.uniqueID = 12;
+        global.globalVars.uniqueID = 13;
         if (!global.globalVars.lastUniqueID || global.globalVars.lastUniqueID != global.globalVars.uniqueID) {
             if (global.globalVars.lastUniqueID)
                 forceReload = true;
